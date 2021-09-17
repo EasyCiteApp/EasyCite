0162e7cd54adbcb02ad96eaf12c2ba5b441637e6
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-09-17 12:34:25 +0700

    Layout for all pages

---
+++ frontend/components/Layout.jsx
@@ -0,0 +1,13 @@
+
+import Footer from './footer/Footer'
+import NavBar from './navbar/NavBar'
+
+export default function Layout({ children }) {
+  return (
+    <div className="container mx-auto">
+      <NavBar/>
+      <main>{children}</main>
+      <Footer/>
+    </div>
+  )
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,13 +2,13 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Easy Cite</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center min-h-screen w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
@@ -66,17 +66,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+      
+    </>
   )
 }
