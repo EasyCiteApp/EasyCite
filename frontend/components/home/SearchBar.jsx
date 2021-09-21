c58844edbbc5e177af38617ad4a5174a0226ac8b
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-09-21 10:28:31 +0700

    Introduce Search bar, Source type and Citation style components

--- frontend/components/home/SearchBar.jsx
+++ frontend/components/home/SearchBar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import CitationType from "./CitationType";
 
 const SearchBar = () => {
   return (
-    <div className=" bg-white h-14 w-3/5 rounded-xl border border-gray-600 mt-10 px-4 py-2  text-gray-400 focus-within:text-gray-600 justify-center items-center">
+    <div className=" bg-white h-14 w-full md:w-3/5 lg:w-3/5 rounded-full border border-gray-600 mt-10 px-4 py-2  text-gray-600 focus-within:text-gray-600 justify-center items-center">
       <form className="flex h-full w-full justify-center items-center focus:ring-0">
+        <CitationType/>
         <input
           type="text"
           name="search"
-          className="h-full w-full px-2 bg-white  border-none focus:ring-0"
+          className="h-full w-full px-2 bg-white  border-none focus:ring-0 focus:outline-none "
           placeholder="Search for anything"
         />
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 ml-2"
+          className="h-6 w-6 ml-2"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
